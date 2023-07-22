5c463a2c9bc02e8f0419ef18087575e664ee0150
tranbaodu1602 <devtranbaodu1602@gmail.com>
2023-07-23 00:23:10 +0700

    build layout and playlist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Login from "./components/Login";
+import Spotify from "./components/Spotify";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/Constants";
 
 export default function App() {
-  return <div>App</div>;
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
+  //
+
+  return <div>{token ? <Spotify /> : <Login />}</div>;
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login() {
+  const handleClick = () => {
+    const clientId = "15d57ae406fe43fab09289321a47803c";
+    const redirectUrl = "http://localhost:3000/";
+    const apiUrl = "https://accounts.spotify.com/authorize";
+    const scope = [
+      "user-read-private",
+      "user-read-email",
+      "user-modify-playback-state",
+      "user-read-playback-state",
+      "user-read-currently-playing",
+      "user-read-recently-played",
+      "user-top-read",
+    ];
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+      " "
+    )}&response_type=token&show_dialog=true`;
+  };
+
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+        alt="spotify"
+      />
+      <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    border: none;
+    color: #49f585;
+    background-color: black;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
