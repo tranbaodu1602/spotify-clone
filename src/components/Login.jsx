72b8e46319cbf084b09b7469e0bc94cb60396955
tranbaodu1602 <devtranbaodu1602@gmail.com>
2023-07-30 21:49:10 +0700

    "update localhost"

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,8 @@ import styled from "styled-components";
 export default function Login() {
   const handleClick = () => {
     const clientId = "15d57ae406fe43fab09289321a47803c";
-    const redirectUrl = "https://spotify-clone-ruby-one.vercel.app/";
+    // const redirectUrl = "https://spotify-clone-ruby-one.vercel.app/";
+    const redirectUrl = "http://localhost:3000/";
     const apiUrl = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
